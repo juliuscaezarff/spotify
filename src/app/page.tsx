5ff494342e14e1f3efd0cbe5aa45f37af9d10d99
julius caezar <julius.caezar25@gmail.com>
2023-08-01 01:00:51 -0400

    feat: Importando o componente heroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { Header } from '@/components/header'
+import { Header } from '@/components/Header'
+import { HeroSection } from '@/components/HeroSection'
 
 export default function Home() {
-  return <Header />
+  return (
+    <>
+    <Header />
+    <HeroSection />
+    </>
+  )
 }
