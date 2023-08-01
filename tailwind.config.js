4fd4fde131c786607e76b8a97e482b4a39f7eac1
julius caezar <julius.caezar25@gmail.com>
2023-08-01 00:44:08 -0400

    feat: Configurando os espaçamento, colors, background e font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,34 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {
+        14: '125'
       },
-    },
+      colors: {
+        hoverspt: '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
+      },
+      backgroundImage: {
+        'spotify-theme': 'url(../assets/bursts.svg)',
+        'spotify-theme-mobile': 'url(../assets/bursts-mobile.svg)'
+      },
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        banner: '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
