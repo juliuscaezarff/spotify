a985b361b0c3a59c311ea476bf7bd11cd851e3e8
julius caezar <julius.caezar25@gmail.com>
2023-08-01 18:16:47 -0400

    feat: criando algumas themes de font-size e colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,24 +8,12 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        14: '125'
+        14: '125',
       },
       colors: {
-        hoverspt: '#18D760',
+        'hoverspt': '#18D760',
         'purple-main': '#2D46B9',
         'green-main': '#1ED760'
-      },
-      backgroundImage: {
-        'spotify-theme': 'url(../assets/bursts.svg)',
-        'spotify-theme-mobile': 'url(../assets/bursts-mobile.svg)'
-      },
-      backgroundSize: {
-        '175%': '175%',
-        '195%': '195%'
-      },
-      backgroundPosition: {
-        banner: '46% 4%',
-        'banner-mobile': 'top 25% center'
       },
       fontSize: {
         '9xl': '9rem'
