842fa672149bae9eec0694dec6cef324f28117b5
julius caezar <julius.caezar25@gmail.com>
2023-08-01 00:24:55 -0400

    feat: first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,53 @@
+interface HeaderProps {}
+export function Header(props: HeaderProps) {
+  return (
+    <header>
+      <div className="flex fixed bg-black w-full px-3 md:px-20 py-4">
+        <div className="container flex items-center mx-auto">
+          <div>
+            <a href="#">
+              <img className="w-20 md:w-[125px]" src="" alt="" />
+            </a>
+          </div>
+          <div className="flex justify-end flex-1 md:hidden text-white text-3xl">
+            <i></i>
+          </div>
+          <div className="items-end flex-1 text-white font-bold hidden md:flex">
+            <nav className="flex-1">
+              <ul className="flex justify-end flex-1">
+                <li className="px-4">
+                  <a className="hover:text-hoverspt text-sm" href="">
+                    Premium
+                  </a>
+                </li>
+                <li className="px-4">
+                  <a className="hover:text-hoverspt text-sm" href="">
+                    Ajuda
+                  </a>
+                </li>
+                <li className="px-4">
+                  <a className="hover:text-hoverspt text-sm" href="">
+                    Baixar
+                  </a>
+                </li>
+                <li className="px-4">
+                  <span className="border-r border-white"></span>
+                </li>
+                <li className="px-4">
+                  <a href="#" className="hover:text-hoverspt text-sm">
+                    Inscrever-se
+                  </a>
+                </li>
+                <li className="px-4">
+                  <a href="#" className="hover:text-hoverspt text-sm">
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
