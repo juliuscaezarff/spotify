e81550a112ec898da2feeb0892c0d0ddccfabc46
julius caezar <julius.caezar25@gmail.com>
2023-08-03 16:20:51 -0400

    importando os components footer e premiumsection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { HeroSection } from '@/components/HeroSection'
+import { PremiumSection } from '@/components/PremiumSection'
 
 export default function Home() {
   return (
     <>
     <Header />
     <HeroSection />
+    <PremiumSection />
+    <Footer />
     </>
   )
 }
